344a634dcb19a9cfba623e1ee4db27f09621c1e8
Sumit Bandyopadhyay <sumitb2015@gmail.com>
2022-07-21 20:20:59 +0530

    Added Python Script

---
+++ practice03.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from pathlib import Path
+
+dirpath = Path.home() / "Desktop"
+
+for file in dirpath.iterdir():
+    if file.suffix == ".xlsx":
+        name = file.stem
+        c = name.split(" ")
+        print(c)
+        d = f'{datetime.now().date()}_{c[1]}_{c[2]}{file.suffix}'
+        new_path = dirpath.joinpath(datetime.strftime(datetime.now().date(),"%B"))    
+        print(new_path)
+        if not new_path.exists():
+            new_path.mkdir()
+        new_path = new_path / d
+        file.rename(new_path)
+        
+        
